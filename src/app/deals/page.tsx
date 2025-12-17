f64ba3079eeb607ca7e64d866d82b420060678ba
Billy Rochester <honesthomesales@gmail.com>
2025-12-17 00:00:03 -0500

    Allow negative values for Soteris_$ field in deals

--- src/app/deals/page.tsx
+++ src/app/deals/page.tsx
@@ -470,7 +470,6 @@ export default function DealsPage() {
                     type="number"
                     name="Soteris_$"
                     step="0.01"
-                    min="0"
                     className="mt-1 block w-full border border-gray-300 rounded-md px-3 py-2"
                   />
                 </div>
